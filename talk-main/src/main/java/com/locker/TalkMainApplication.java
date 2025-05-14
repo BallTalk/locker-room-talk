d1cd611fbcc66476a847ecbfd26094756c2dcebb
노영오 <nohyo05@naver.com>
2025-05-14 21:49:31 +0900

    build: 프로젝트 기본 구성 세팅

---
+++ talk-main/src/main/java/com/locker/TalkMainApplication.java
@@ -0,0 +1,13 @@
+package com.locker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TalkMainApplication.class, args);
+    }
+
+}
