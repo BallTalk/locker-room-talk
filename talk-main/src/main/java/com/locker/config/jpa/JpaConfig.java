d260ac8f977359f00920ec4de5f86b114b2b0582
Oh Hyoung Seok <gudtjr1355@gmail.com>
2025-05-19 19:54:38 +0900

    build(config): 개발 환경 설정을 위한 common 및 config 패키지 및 파일 추가
    - BaseEntity 생성
    - 개발환경 로깅을 위한 configs, aop, filter, interceptor 기본 환경 추가
    - schema.sql, data.sql 수정

---
+++ talk-main/src/main/java/com/locker/config/jpa/JpaConfig.java
@@ -0,0 +1,10 @@
+package com.locker.config.jpa;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@Configuration
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = "com.locker")
+public class JpaConfig {}
