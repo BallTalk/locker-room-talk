def49fd5d9a15c1cb27a1174fec1337cb7befdd8
Oh Hyoung Seok <gudtjr1355@gmail.com>
2025-05-20 21:35:21 +0900

    feat: 공통 에러코드, GlobalExceptionHandler, CustomException 추가
    - UserException 은 수정 예정
    - schema.sql ENUM 자료형 CHECK 로 변경

--- talk-main/src/main/resources/sql/schema.sql
+++ talk-main/src/main/resources/sql/schema.sql
@@ -3,20 +3,22 @@ DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user` (
                         id                 BIGINT        AUTO_INCREMENT PRIMARY KEY COMMENT '유저 PK ID',
                         login_id           VARCHAR(50)   NULL UNIQUE COMMENT '아이디',
-                        provider           ENUM('LOCAL','GOOGLE','KAKAO') NOT NULL DEFAULT 'LOCAL' COMMENT '가입 방식',
+                        provider           VARCHAR(10)   NOT NULL DEFAULT 'LOCAL' COMMENT '가입 방식',
+                                                         CHECK (provider IN ('LOCAL','GOOGLE','KAKAO')),
                         provider_user_id   VARCHAR(100)  NULL                COMMENT 'OAuth 공급자 유저 ID',
                         password           VARCHAR(255)  NOT NULL            COMMENT '패스워드',
                         nickname           VARCHAR(50)   NOT NULL            COMMENT '닉네임',
                         favorite_team_id   VARCHAR(50)   NOT NULL            COMMENT '유저 응원팀 ID',
                         profile_image_url  VARCHAR(255)  NULL                COMMENT '프로필 이미지 URL', -- 임시
                         status_message     VARCHAR(200)  NULL                COMMENT '한 줄 상태 메시지', -- 임시
-                        status             ENUM(
-                                                   'ACTIVE',
-                                                   'SUSPENDED',
-                                                   'BANNED',
-                                                   'WITHDRAWN',
-                                                   'DORMANT'
-                                                ) NOT NULL DEFAULT 'ACTIVE' COMMENT '상태',
+                        status             VARCHAR(10)  NOT NULL DEFAULT 'ACTIVE' COMMENT '상태',
+                                                        CHECK (status IN (
+                                                                      'ACTIVE',
+                                                                      'SUSPENDED',
+                                                                      'BANNED',
+                                                                      'WITHDRAWN',
+                                                                      'DORMANT'
+                                                        )),
                         last_login_at      DATETIME      NULL                COMMENT '마지막 로그인 날짜',
                         login_fail_count   INT           NOT NULL DEFAULT 0  COMMENT '로그인 실패 횟수',
                         deleted_at         DATETIME      NULL                COMMENT '탈퇴 시 탈퇴 날짜',
