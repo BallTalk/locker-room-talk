f887afeb907b1b3b9441d38a00a5a32cb0e6228b
dhgudtmxhs <gudtjr1355@gmail.com>
2025-05-26 17:42:43 +0900

    feat: JWT 기반 로그인·로그아웃, OAuth 로그인 구현
    - Login: 사용자 인증 시 액세스·리프레시 토큰 발급
    - Logout: 토큰 만료 전에도 재사용될 위험이 있어, 로그아웃 시 해당 JWT를 Redis 블랙리스트에 저장해 즉시 무효화
    - OAuth: JWT를 사용할 수 있게끔 구현

---
+++ talk-main/src/main/java/com/locker/auth/temp/LoginRequest.java
@@ -0,0 +1,15 @@
+package com.locker.auth.temp;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+
+@Schema(description = "로그인 요청 DTO")
+public record LoginRequest(
+        @Schema(description = "로그인 아이디", example = "user01")
+        @NotBlank(message = "LOGIN_ID_REQUIRED")
+        String loginId,
+
+        @Schema(description = "비밀번호", example = "P@ssw0rd!")
+        @NotBlank(message = "PASSWORD_REQUIRED")
+        String password
+) {}
---
+++ talk-main/src/main/java/com/locker/common/exception/specific/AuthException.java
@@ -0,0 +1,19 @@
+package com.locker.common.exception.specific;
+
+import com.locker.common.exception.base.CustomException;
+import com.locker.common.exception.model.ErrorCode;
+
+public class AuthException extends CustomException {
+    private AuthException(ErrorCode code) {
+        super(code);
+    }
+
+    public static AuthException authenticationFailed() {
+        return new AuthException(ErrorCode.AUTHENTICATION_FAILED);
+    }
+
+    /*public static AuthException tokenBlacklisted() {
+        return new AuthException(ErrorCode.TOKEN_BLACKLISTED);
+    }*/
+
+}
